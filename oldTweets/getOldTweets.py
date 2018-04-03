a2485d6f999a5ec4991870cab5d4ffdc40ff16b1
Temirlan97 <t.ulugbekuulu@jacobs-university.de>
2018-04-03 16:36:40 +0200

    added timed logging to getOldTweets

--- oldTweets/getOldTweets.py
+++ oldTweets/getOldTweets.py
@@ -1,9 +1,28 @@
 import codecs
 import sys
+import time
 
 sys.path.append("./GetOldTweets-python")
 
 import got
+#################################
+# LOGGING						
+#################################
+def Log(output, filename):
+	#First prepare the output
+	readyOutput = str(time.asctime(time.localtime())) + ":\n"
+	for line in str(output).split("\n"):
+		readyOutput += "\t" + line + "\n"
+	#then print and write to the file
+	print(readyOutput)
+	with open(filename, "a") as logfile:
+		logfile.write(readyOutput)
+
+def errorLog(output):
+	Log(output, "errorlog.txt");
+
+def infoLog(output):
+	Log(output, "infolog.txt");
 
 tweetCriteria = got.manager.TweetCriteria()
 outputFileName = "old_tweets.csv"
@@ -21,16 +40,16 @@ outputFile = codecs.open(outputFileName, "a", "utf-8")
 
 outputFile.write("username;date;text;id")
 
-print("Searching...")
+infoLog("Searching...")
 
 def receiveBuffer(tweets):
 	for t in tweets:
 		outputFile.write(('\n%s;%s;"%s";"%s"' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.text, t.id)))
 	outputFile.flush()
-	print('More %d saved on file...\n' % len(tweets))
+	infoLog('More ' + str(len(tweets)) + ' saved on file...')
 
 got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
 
 outputFile.close()
-print("Done!")
+infoLog("Done!")
 
