3f2b643e215c011a01794bc7b10d293c5381e8ec
Temirlan97 <t.ulugbekuulu@jacobs-university.de>
2018-04-19 13:33:10 +0200

    reimplemented writeIntoDb using pandas

--- processOld/writeIntoDb.py
+++ processOld/writeIntoDb.py
@@ -1,5 +1,6 @@
 #this program writes the processed grades into the database
 import csv, sys, MySQLdb
+import pandas as pd
 #import utils
 sys.path.append("../myutils/")
 from log import errorLog, infoLog
@@ -12,25 +13,27 @@ db = MySQLdb.connect(host = configs['mysqlHost'], user = configs['mysqlUser'], p
 insertQuery = "INSERT INTO twitter.tweets(timestamp, tweet_id, score)" \
 	"VALUES(%s,%s,%s) ON DUPLICATE KEY UPDATE tweet_id=tweet_id;"
 try:
-	with open("processedTweets.csv", "r") as csvFile:
-		reader = csv.reader(csvFile, delimiter = ';')
-		reader.next()
-		cursor = db.cursor()
-		count = 0
-		for row in reader:
-			#username;timestamp;tweetid;score
-			timestamp = row[1]
-			tweetId = row[2]
-			score = float(row[3])
-			if(score != 0.0):
-				args = (timestamp, tweetId, score)
-				count += 1
-				cursor.execute(insertQuery, args)
-				if(count % 1000 == 0):
-					infoLog(count)
-		db.commit()
-		cursor.close()
-	notifyViaEmail("Writing Old tweets finished", "Terminated Normally")
+	csvFile = pd.read_csv("processedTweets.csv", delimiter=";")
+	cursor = db.cursor()
+	count = 0
+	totalCount = 0
+	for index, row in csvFile.iterrows():
+		#username;timestamp;tweetid;score
+		score = float(row['score'])
+		if(score != 0.0):
+			args = (int(row['timestamp']), int(row['tweetid']), float(row['score']))
+			count += 1
+			cursor.execute(insertQuery, args)
+			if(count >= 1000):
+				totalCount += count
+				count = 0
+				infoLog(totalCount)
+				db.commit()
+	db.commit()
+	cursor.close()
+	terminationMessage = "Terminated Normally. Inserted " + str(totalCount) + " items into the DB"
+	infoLog(terminationMessage)
+	notifyViaEmail("Writing Old tweets finished", terminationMessage)
 except Exception as error:
 	errorLog(error)
 	notifyViaEmail("CRASHED Writing Old tweets into DB", str(error))
